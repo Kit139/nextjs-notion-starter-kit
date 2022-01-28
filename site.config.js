c672373b0e73f2aba0f8de5ee3884f825071a161
Kit139 <57795217+Kit139@users.noreply.github.com>
2022-01-28 18:57:09 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '5425086a912246948ac7d422cf03b3be',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: 'efe95adb-69a0-4a61-a702-636d22e48e41',
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'kit’s blog',
+  domain: '',
+  author: 'kit',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,14 +17,14 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'Kit139',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
+  defaultPageIcon: 'https://zhangkaiheng.gitee.io/image/avatar.png',
+  defaultPageCover: 'https://gitee.com/cit_k/pictures/raw/master/picture/21%202.jpg',
   defaultPageCoverPosition: 0.5,
 
   // image CDN host to proxy all image requests through (optional)
